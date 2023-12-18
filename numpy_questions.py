9a7503e011c805c292b42a7798ff7ca6e104cd18
Nirunoc <cyprien.raffi@gmail.com>
2023-12-18 16:57:50 +0100

    passing tests

--- numpy_questions.py
+++ numpy_questions.py
@@ -42,11 +42,11 @@ def max_index(X):
 
     # TODO
 
-    if not isinstance(X,np.ndarray):
+    if not isinstance(X, np.ndarray):
         raise ValueError
     if len(X.shape) != 2:
         raise ValueError
-    (i,j) = np.unravel_index(X.argmax(), X.shape)
+    (i, j) = np.unravel_index(X.argmax(), X.shape)
     return i, j
 
 
@@ -69,7 +69,7 @@ def wallis_product(n_terms):
     """
     # XXX : The n_terms is an int that corresponds to the number of
     # terms in the product. For example 10000.
-    array_of_n = np.arange(1,n_terms+1,1,dtype=np.longlong)
+    array_of_n = np.arange(1, n_terms+1, 1, dtype=np.longlong)
     array_of_4_n2 = 4*array_of_n*array_of_n
     wallis_terms = array_of_4_n2/(array_of_4_n2-1)
     return 2*np.product(wallis_terms)
